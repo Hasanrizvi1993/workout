97c9258e45bff81ee91f2ea97f6dea409ccdda56
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-08-30 20:11:02 -0700

    server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const routes = require("./routes/appRoutes");
 
 const PORT = process.env.PORT || 4000;
 
+app.use(express.static("static"));
 app.use(express.json());
 
 //to view ejs
@@ -16,15 +17,15 @@ app.set("view engine", "ejs");
 //middleware
 //to use the routes
 app.use(routes);
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/public", express.static(__dirname + "/public"));
-
 
 // app.get('/haha', function(req, res) {
 //     res.sendFile(path.join(__dirname, './views/haha.html'));
 //   });
-  
-
 
 //listen to this port
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(process.env.PORT || PORT, () =>
+	console.log(`listening on port ${PORT}`),
+);
